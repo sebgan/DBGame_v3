cebbcb4d91f86c370ddd8aff778eb1a10110af28
Seb <sebastian.gandso@gmail.com>
2015-12-17 13:28:46 +0100

    Ændring

--- src/DBGame_v3.java
+++ src/DBGame_v3.java
@@ -6,18 +6,27 @@ public class DBGame_v3 {
     GameConnection connection;
 
     public DBGame_v3() {
+        //connection = new GameConnection("wits.ruc.dk", "3306", "gandsoe", "gandsoe", "oQevALOP");
         connection = new GameConnection("localhost", "3306", "version2", "root", "");
-        //connection = new Database("localhost", "3306", "version2", "root", "");
 
-        //System.out.println("Udfører updatePiece");
-        //connection.updatePiece(3,"x",1);
+        if(connection.isValid) {
 
-        Controller controller = new Controller(connection);
-        David david = new David(controller);
+            Controller controller = new Controller(connection);
+            David david = new David(controller);
 
-        translator = new GameTranslator(connection);
-        presentation = new GamePresentation(translator, david, 800, 600);
-        presentation.startRenderLoop();
+            translator = new GameTranslator(connection);
+
+            Behavior behavior = new Behavior(controller, "follow");
+            NPC npc = new NPC(translator, controller, behavior, 2);
+
+            presentation = new GamePresentation(translator, david, 800, 600);
+            presentation.setNPC(npc);
+            presentation.startRenderLoop();
+
+        }
+        else {
+            System.out.println("Kunne ikke starte spillet da connection mangler.");
+        }
 
     }
 
